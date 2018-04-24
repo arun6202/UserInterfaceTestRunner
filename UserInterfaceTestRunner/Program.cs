7d3f368ba1579cf4395253ed18b287a251d31c0e
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-04-24 14:28:42 +0530

    intial push

---
+++ UserInterfaceTestRunner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using XamarinFormsStarterKit.UserInterfaceVisualizer;
+
+namespace XamarinFormsStarterKit.UserInterfaceTestRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            AppiumRunner.Execute();
+            Console.WriteLine("Reached End!");
+            Console.Read();
+        }
+    }
+}
